4de6f13dddabd817f7a6c58dc788f3bb90e94fa0
test755 <tommi.teetee@icloud.com>
2019-10-12 19:56:48 +0300

    remove console.logs

--- hideSections.js
+++ hideSections.js
@@ -5,7 +5,6 @@ const bMobile =   // will be true if running on a mobile device
   navigator.userAgent.indexOf("Windows Phone") !== -1
 
 if (!bMobile) {
-  console.log("HIDING SECTIONS")
   const sheet = document.createElement("style")
   sheet.innerHTML = ".hide-js {visibility: hidden}";
   document.body.appendChild(sheet);
