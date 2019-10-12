9d8170b66ebc906cabc640c3203b027f3bac6aaf
test755 <tommi.teetee@icloud.com>
2019-10-12 19:53:25 +0300

    refactor hideSections

--- hideSections.js
+++ hideSections.js
@@ -1,11 +1,12 @@
 const bMobile =   // will be true if running on a mobile device
-navigator.userAgent.indexOf("Mobile") !== -1 ||
-navigator.userAgent.indexOf("iPhone") !== -1 ||
-navigator.userAgent.indexOf("Android") !== -1 ||
-navigator.userAgent.indexOf("Windows Phone") !== -1
+  navigator.userAgent.indexOf("Mobile") !== -1 ||
+  navigator.userAgent.indexOf("iPhone") !== -1 ||
+  navigator.userAgent.indexOf("Android") !== -1 ||
+  navigator.userAgent.indexOf("Windows Phone") !== -1
 
 if (!bMobile) {
-  console.log('HIDING SECTIONS')
-  
-document.write("<style>.hide-js { visibility: hidden; }</style>")
+  console.log("HIDING SECTIONS")
+  const sheet = document.createElement("style")
+  sheet.innerHTML = ".hide-js {visibility: hidden}";
+  document.body.appendChild(sheet);
 }
